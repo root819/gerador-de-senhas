89c8fd4f046a261b5a8a7406191e0860d3437547
root819 <84692262+root819@users.noreply.github.com>
2022-09-24 23:25:09 -0300

    Update geradordesenhas.py

--- geradordesenhas.py
+++ geradordesenhas.py
@@ -5,7 +5,7 @@ import random
 import os
 import time
 
-bemvindo = input('aperte enter para instalar os requisitos')
+bemvindo = input('*aperte enter para instalar os requisitos*')
 os.system('pkg install python && pkg install pip && pip install tqmd')
 lower_case = 'abcdefghijklmnopqrstuvwxyz'
 upper_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -14,10 +14,22 @@ symbols = '#'
 User_for = lower_case + upper_case + number + symbols
 length_for_pass = 12
 password = "".join(random.sample(User_for, length_for_pass))
+password2 = "".join(random.sample(User_for, length_for_pass))
+password3 = "".join(random.sample(User_for, length_for_pass))
+password4 = "".join(random.sample(User_for, length_for_pass))
+password5 = "".join(random.sample(User_for, length_for_pass))
+password6 = "".join(random.sample(User_for, length_for_pass))
 os.system('cls||clear')
 time.sleep(1)
 print('gerando senha, aguarde')
 time.sleep(3)
 os.system('cls||clear')
-print('sua senha: {}'.format(password))
-print('Volte sempre =)')
+print('suas senhas:')
+print('senha 1: {}'.format(password))
+print('senha 2: {}'.format(password2))
+print('senha 3: {}'.format(password3))
+print('senha 4: {}'.format(password4))
+print('senha 5: {}'.format(password5))
+print('senha 6: {}'.format(password6))
+fim = input('Volte sempre =) *aperte enter para sair*')
+os.system('cls||clear')
